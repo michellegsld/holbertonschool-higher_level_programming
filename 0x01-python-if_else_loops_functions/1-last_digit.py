2480782cbde0cbf6772c9baf31aa35a03a698f77
michelle <michellegsld@gmail.com>
2019-12-10 14:11:26 -0500

    4th Pushing, Using another variable for last digit, Not hard writing the '-' and removing use of abs()

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:d} is ".format(number), end = '')
-if number < 0 and number % 10 != 0:
-    print("-", end = '')
-print("{:d} ".format(abs(number) % 10), end = '')
-if abs(number) % 10 > 5 and number > 0:
+if number < 0:
+    last = number % -10
+else:
+    last = number % 10
+print("Last digit of {:d} is {:d} ".format(number, last), end = '')
+if last > 5:
     print("and is greater than 5")
-elif number % 10 == 0:
+elif last == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
