d9933d69dd090acae4dce36a1ae359dc2c93bb5b
michelle <michellegsld@gmail.com>
2019-12-10 15:01:10 -0500

    2nd Pushing, Accidentally used 3 print() instead of 2

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@ def uppercase(str):
     for check in str:
         for letter in range(97, 123):
             if letter == ord(check):
-                print("{:c}".format(letter - 32), end='')
-                break
-        if letter == 122:
-            print("{:s}".format(check), end='')
+                check = chr(letter - 32)
+        print("{:s}".format(check), end='')
     print("")
