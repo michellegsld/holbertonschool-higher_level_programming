25c073f11a55520af524f36b70420cb8cbd3fe73
michelle <michellegsld@gmail.com>
2019-12-10 14:21:37 -0500

    6th Pushing, Removing spaces around '=' for pep8 requirements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ if number < 0:
     last = number % -10
 else:
     last = number % 10
-print("Last digit of {:d} is {:d} ".format(number, last), end = '')
+print("Last digit of {:d} is {:d} ".format(number, last), end='')
 if last > 5:
     print("and is greater than 5")
 elif last == 0:
