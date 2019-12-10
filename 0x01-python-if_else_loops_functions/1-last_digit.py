14ff63a99836d56436bc487f72722f7a0336b975
michelle <michellegsld@gmail.com>
2019-12-10 14:05:14 -0500

    3rd Pushing, Fix error that wasn't printing '-'

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:d} is {:d} ".format(number, number % 10), end = '')
-if number % 10 > 5:
+print("Last digit of {:d} is ".format(number), end = '')
+if number < 0 and number % 10 != 0:
+    print("-", end = '')
+print("{:d} ".format(abs(number) % 10), end = '')
+if abs(number) % 10 > 5 and number > 0:
     print("and is greater than 5")
 elif number % 10 == 0:
     print("and is 0")
