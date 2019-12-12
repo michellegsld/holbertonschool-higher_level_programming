8792bd4a6cef542b540e43338804b7596ebeff98
michelle <michellegsld@gmail.com>
2019-12-12 16:17:05 -0500

    4th Pushing, Moving placement of import

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 from sys import argv
 from calculator_1 import add, sub, mul, div
-if __name__ == "__main__":
     argc = len(argv) - 1
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
