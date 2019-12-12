3e95acad9508e181e0bc6de02849cca71628de13
michelle <michellegsld@gmail.com>
2019-12-11 20:12:17 -0500

    6th Pushing, Simplifying and removing variable previous

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,31 +12,39 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new;
-	listint_t *prev = *head;
-	listint_t *current = (*head)->next;
+	listint_t *current = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+
+	new->n = number;
 
 	if (*head == NULL)
 	{
 		new->next = *head;
 		*head = new;
 	}
+	else if (current->next == NULL)
+	{
+		if (current->n > number)
+		{
+			new->next = current;
+			*head = new;
+		}
+		else
+		{
+			current->next = new;
+			new->next = NULL;
+		}
+	}
 	else
 	{
-		while ((current != NULL) && (current->n < number))
-		{
-			prev = prev->next;
+		while (current->next && current->next->n < number)
 			current = current->next;
-		}
-
-		new->next = current;
-		prev->next = new;
+		new->next = current->next;
+		current->next = new;
 	}
-
-	new->n = number;
 
 	return (new);
 }
