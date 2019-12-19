b66ffd87f49a1be1d155505ce29a5487203c83d8
michelle <michellegsld@gmail.com>
2019-12-18 23:56:16 -0500

    4th Pushing, Fixing a typo

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ import hidden_4
 if __name__ == "__main__":
     words = dir(hidden_4)
     for i in range(len(words)):
-        if words[i][0] != "_" and words[i][0] != "_":
+        if words[i][0] != "_" and words[i][1] != "_":
             print("{:s}".format(name))
