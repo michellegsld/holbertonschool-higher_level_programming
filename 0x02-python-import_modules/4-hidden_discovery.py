99f7badebc4d7021bd34a81690f7883ef8bc7b9a
michelle <michellegsld@gmail.com>
2019-12-18 23:59:01 -0500

    5th Pushing, Fixing variable typo

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     words = dir(hidden_4)
     for i in range(len(words)):
         if words[i][0] != "_" and words[i][1] != "_":
-            print("{:s}".format(name))
+            print("{:s}".format(words))
