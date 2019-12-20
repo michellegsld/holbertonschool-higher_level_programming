3d71bca0f422028f31737bc842a924c5c979de30
michelle <michellegsld@gmail.com>
2019-12-19 20:41:35 -0500

    2nd Pushing, Trying to pass pep8 check by setting a shorter variable name equal to original dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    return [print("%s: %s" % (k, a_dictionary[k])) for k in sorted(a_dictionary.keys())]
+    a_dic = a_dictionary
+    return [print("%s: %s" % (k, a_dic[k])) for k in sorted(a_dic.keys())]
