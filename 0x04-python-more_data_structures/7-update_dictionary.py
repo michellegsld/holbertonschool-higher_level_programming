d76028295bd8db61a0beaa868adad2dd3eccfa38
michelle <michellegsld@gmail.com>
2019-12-19 21:37:30 -0500

    2nd Pushing, Fulfilling pep8 requirements

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    return a_dictionary.update(key=value) if key in a_dictionary else {**a_dictionary, **{key : value}}
+    a_d = a_dictionary
+    return a_d.update(key=value) if key in a_d else {**a_d, **{key: value}}
