0e9503f8370f6cc41c95be8d7a15660b2529c89f
michelle <michellegsld@gmail.com>
2019-12-19 21:52:40 -0500

    4th Pushing, Changing else statement to add a new key a different way

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_d = a_dictionary
-    if key in a_d:
-        a_d.update(key=value)
+    if key in a_dictionary:
+        a_dictionary.update(key=value)
     else:
-        a_d = {**a_d, **{key: value}}
-    return a_d
+        a_dictionary[key] = value
+    return a_dictionary
