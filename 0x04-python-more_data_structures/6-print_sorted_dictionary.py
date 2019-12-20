88a349792b46d1e0c7cc217fa84c9758928957d9
Michelle Giraldo <michellegsld@gmail.com>
2019-12-20 10:18:48 -0500

    3rd Pushing, Removing return

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     a_dic = a_dictionary
-    return [print("%s: %s" % (k, a_dic[k])) for k in sorted(a_dic.keys())]
+    [print("%s: %s" % (k, a_dic[k])) for k in sorted(a_dic.keys())]
