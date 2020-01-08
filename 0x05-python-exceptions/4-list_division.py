0e6b5b0dcd4182c57fb548bda845b9b1ccae02e9
Michelle Giraldo <michellegsld@gmail.com>
2020-01-08 15:03:30 -0500

    1st Pushing, Using multiple exceptions, 'TypeError' 'ZeroDivisionError' 'IndexError', Creating a new list and appending division or 0 to it

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    new_list = []
+    while i < list_length:
+        try:
+            total = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            total = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            total = 0
+        except IndexError:
+            print("out of range")
+            total = 0
+        finally:
+            new_list.append(total)
+        i += 1
+    return new_list
