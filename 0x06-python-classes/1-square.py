ea009378ee3860b75ed555a535d44449f57ce5b8
Michelle Giraldo <michellegsld@gmail.com>
2020-01-09 17:02:05 -0500

    6th Pushing, Adding the extra '_' before 'size' and removing the '_Square' part

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,4 +3,4 @@ class Square:
     """Represents a square with a size."""
     def __init__(self, size):
         """Initializes the size."""
-        self.__Square__size = size
+        self.___size = size
