1233923d87fb9c32d7b478d54d64ee4ed19ce249
Michelle Giraldo <michellegsld@gmail.com>
2020-01-09 16:48:21 -0500

    2nd Pushing, Adding if statement, Setting object's private attribute size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """Represents a square with a size."""
-
-    def __init__(self, size):
+    def __init__(self, size=None):
         """Initializes the size."""
+        if size is not None:
+            self._size = size
