c6b8ddee7912bc517633214ad3b946af32c0f589
Michelle Giraldo <michellegsld@gmail.com>
2020-01-10 13:55:09 -0500

    2nd Pushing, Adding second for-loop for second position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,27 +3,16 @@ class Square:
     """Represents a square with a size."""
     def __init__(self, size=0, position=(0, 0)):
         """Initializes private instance attribute: size."""
-        try:
-            if type(size) is not int:
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-            else:
-                self.__size = size
-        except TypeError:
-            print("size must be an integer", end='')
-            raise
-        except ValueError:
-            print("size must be >= 0", end='')
-            raise
-        try:
-            if type(position) is not tuple or len(position) is not 2:
-                raise TypeError
-            else:
-                self.__position = position
-        except TypeError:
-            print("position must be a tuple", end='')
-            raise
+        if type(size) is not int:
+            raise TypeError("size must be an integer", end='')
+        elif size < 0:
+            raise ValueError("size must be >= 0", end='')
+        else:
+            self.__size = size
+        if type(position) is not tuple or len(position) is not 2:
+            raise TypeError("position must be a tuple", end='')
+        else:
+            self.__position = position
 
     def area(self):
         """Returns the current square area."""
@@ -37,27 +26,22 @@ class Square:
     @size.setter
     def size(self, value):
         """A property setter to set size."""
-        try:
-            if type(value) is not int:
-                raise TypeError
-            elif value < 0:
-                raise ValueError
-            else:
-                self.__size = value
-        except TypeError:
-            print("size must be an integer", end='')
-            raise
-        except ValueError:
-            print("size must be >= 0", end='')
-            raise
+        if type(value) is not int:
+            raise TypeError("size must be an integer", end='')
+        elif value < 0:
+            raise ValueError("size must be >= 0", end='')
+        else:
+            self.__size = value
 
     def my_print(self):
         """Prints in stdout the square."""
         if self.__size is 0:
             print()
         else:
+            for y in range(0, self.__position[1]):
+                print()
             for i in range(0, self.__size):
-                for s in range(0, self.__position[0]):
+                for x in range(0, self.__position[0]):
                     print(" ", end='')
                 for j in range(0, self.__size):
                     print("#", end='')
