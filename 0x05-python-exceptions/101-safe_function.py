97ff0d4e970b596e7a3f9b4c58717ac0c6ba6bc6
Michelle Giraldo <michellegsld@gmail.com>
2020-01-10 10:48:19 -0500

    1st Pushing, Calling a function and sending args as a parameter, Return the result of the function or return None in except

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        answer = fct(*args)
+        return answer
+    except Exception as msg:
+        print("{}".format(msg), file=stderr)
+        return None
