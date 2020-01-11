3d060536da65f9e6758ebeb3dbba4cf710ac6167
Michelle Giraldo <michellegsld@gmail.com>
2020-01-11 16:40:05 -0500

    1st Pushing, Added docstrings, Two if-statements to raise a TypeError

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+0-add_integer.py
+add_integer(a, b=98)
+tests/0-add_integer.txt
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers, else raises a TypeError
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
