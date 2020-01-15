555fa9d08e8ca5daf8aa6e2dc583ccefac96c78d
Michelle Giraldo <michellegsld@gmail.com>
2020-01-14 21:50:31 -0500

    1st Pushing, Creating a method with __repr__

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+4-rectangle.py
+class Rectangle
+"""
+
+
+class Rectangle:
+    """Represents a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initializes private instance attributes: width and height"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Retrieves the private instance attribute: width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """A property setter to set width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves the private instance attribute: height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """A property setter to set height"""
+        self.__height = value
+
+    def area(self):
+        """Retrieves the area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Retrieves the perimeter"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Returns the string that prints the Rectangle"""
+        rec = ''
+        if self.__height > 0 and self.__width > 0:
+            for i in range(self.__height):
+                rec = rec + ('#' * self.__width) + '\n'
+        return rec[:-1]
+
+    def __repr__(self):
+        """Returns a string representation of the Rectangle"""
+        sw, sh = str(self.__width), str(self.__height)
+        return "Rectangle(" + sw + ", " + sh + ")"
