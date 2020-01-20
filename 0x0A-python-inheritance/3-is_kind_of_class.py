f0ce14c119ec760ade00509363e4cda65821589b
Michelle Giraldo <michellegsld@gmail.com>
2020-01-20 14:12:06 -0500

    1st Pushing, Using isinstance()

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+3-is_kind_of_class.py
+is_kind_of_class(obj, a_class):
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks if an object is an instance of, or inherited from, a specified class
+    """
+    return(isinstance(obj, a_class))
