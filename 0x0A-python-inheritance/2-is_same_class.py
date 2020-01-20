297fa24d07bcda533b5772144e7f50564b69654f
Michelle Giraldo <michellegsld@gmail.com>
2020-01-20 13:57:54 -0500

    1st Pushing, Uses isinstance() to check if an object is an instance of a specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+2-is_same_class.py
+def is_same_class(obj, a_class)
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is exactly an instance of a specified class
+    """
+    return(isinstance(obj, a_class))
