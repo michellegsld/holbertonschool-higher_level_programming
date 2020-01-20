10ae02cbc246443aca3422272d828c8d38955650
Michelle Giraldo <michellegsld@gmail.com>
2020-01-20 15:32:48 -0500

    1st Pushing, Making a class with a method that prints a sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+1-my_list.py
+MyList(list)
+"""
+
+
+class MyList(list):
+    """
+    A class that inherits from list
+    """
+    def print_sorted(self):
+        """
+        Prints the list in ascended sort
+        """
+        print("{}".format(sorted(self)))
