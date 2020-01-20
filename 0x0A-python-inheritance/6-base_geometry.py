698d1a3397e8868838ab3c2e1727a279154c35e8
Michelle Giraldo <michellegsld@gmail.com>
2020-01-20 14:51:56 -0500

    1st Pushing, Creating area() method that raises an error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+6-base_geometry.py
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Adds an area() method
+    """
+    def area(self):
+        """
+        Raises an exception error
+        """
+        raise Exception("area() is not implemented")
