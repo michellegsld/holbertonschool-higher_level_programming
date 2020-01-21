bb433d5cbc3035352d54655136c6d11c3a4bb22e
Michelle Giraldo <michellegsld@gmail.com>
2020-01-21 15:02:14 -0500

    1st Pushing, Using with to open a file and print each line as read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0-read_file.py
+read_file(filename="")
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it line by line
+    """
+    with open(filename) as fil:
+        for line in fil:
+            print(line, end='')
