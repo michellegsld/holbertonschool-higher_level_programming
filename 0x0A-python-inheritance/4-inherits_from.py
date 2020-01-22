b2241d64b37c3cf2fa1686f91c3023c7e00d487b
Michelle Giraldo <michellegsld@gmail.com>
2020-01-22 11:29:31 -0500

    2nd Pushing, Doing correct usage

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     """
     Check if obj is an instance of a_class that was (in)directly inherited
     """
-    return(not issubclass(a_class, type(obj)))
+    return(issubclass(type(obj), a_class) and type(obj) is not a_class)
