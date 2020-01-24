f353520ec0cc49a39b16cb08109828b09edd39e1
Michelle Giraldo <michellegsld@gmail.com>
2020-01-24 15:32:41 -0500

    1st Pushing, Creating Base class that sets the an instance's id

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+base.py
+class Base()
+"""
+
+
+class Base:
+    """
+    The most super class to manage id attribute
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        Either set id or increment __nb__objects
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
