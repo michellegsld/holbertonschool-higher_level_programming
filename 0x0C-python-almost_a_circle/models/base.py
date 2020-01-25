877c90c2d4f0187effab501be553b6796f332f0c
Michelle Giraldo <michellegsld@gmail.com>
2020-01-25 15:00:18 -0500

    7th Pushing, Adding classmethod create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,3 +52,12 @@ class Base:
         if json_string is None or not json_string:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        Returns an instance with all attributes already set
+        """
+        instance = cls(1, 1)
+        instance.update(**dictionary)
+        return instance
