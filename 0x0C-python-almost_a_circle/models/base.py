9bb9116d057be392be11e80f12b73b1065847165
Michelle Giraldo <michellegsld@gmail.com>
2020-01-25 00:20:42 -0500

    4th Pushing, Adding condition to to_json_string method, Accounts for if list_dictionaries is None or empty

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,4 +27,6 @@ class Base:
         """
         Returns a JSON string representation
         """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         return json.dumps(list_dictionaries)
