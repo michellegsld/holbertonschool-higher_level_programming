acca9a17aa7f105c7c2cbf3278c0effe0cb355ee
Michelle Giraldo <michellegsld@gmail.com>
2020-01-25 15:56:39 -0500

    8th Pushing, Adding load_from_file method, Shortening some methods to be more pythonic

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,8 +3,10 @@
 base.py
 class Base
 importing json
+importing os.path
 """
 import json
+import os.path
 
 
 class Base:
@@ -27,10 +29,14 @@ class Base:
     def to_json_string(list_dictionaries):
         """
         Returns a JSON string representation
-        """
+        -- before had:
         if list_dictionaries is None or not list_dictionaries:
             return json.dumps("[]")
         return json.dumps(list_dictionaries)
+        -- now:
+        """
+        ld = list_dictionaries
+        return (json.dumps("[]") if ld is None or not ld else json.dumps(ld))
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -48,10 +54,14 @@ class Base:
     def from_json_string(json_string):
         """
         Returns the list of the JSON string representation
-        """
+        -- before:
         if json_string is None or not json_string:
             return []
         return json.loads(json_string)
+        -- now:
+        """
+        js = json_string
+        return ([] if js is None or not js else json.loads(js))
 
     @classmethod
     def create(cls, **dictionary):
@@ -61,3 +71,16 @@ class Base:
         instance = cls(1, 1)
         instance.update(**dictionary)
         return instance
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Returns a list of instances
+        """
+        filename = str(cls.__name__) + ".json"
+        jsonlist = []
+        if not os.path.isfile(filename):
+            return []
+        with open(filename) as fil:
+            jsonlist = cls.from_json_string(fil.read())
+        return [cls.create(**item) for item in jsonlist]
