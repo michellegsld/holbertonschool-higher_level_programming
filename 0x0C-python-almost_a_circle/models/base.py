ef04d6d614f72b9debaf974e5ebcc306b02bb205
Michelle Giraldo <michellegsld@gmail.com>
2020-01-25 00:19:31 -0500

    3rd Pushing, Adding to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,14 +2,17 @@
 """
 base.py
 class Base
+importing json
 """
+import json
 
 
 class Base:
     """
-    Main purpose: manage the id attribute
+    Main purpose: manage the id attribute and handle json
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
         Either set id or increment __nb__objects
@@ -19,3 +22,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        Returns a JSON string representation
+        """
+        return json.dumps(list_dictionaries)
