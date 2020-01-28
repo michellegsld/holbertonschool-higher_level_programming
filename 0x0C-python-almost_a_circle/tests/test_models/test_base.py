30c2e97296cefdb9186b4c39e0d51297623066c9
Michelle Giraldo <michellegsld@gmail.com>
2020-01-27 22:36:26 -0500

    1st Pushing, Basic tests checking id attribute

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Unittest for base.py
+"""
+import unittest
+from models.base import Base
+
+class TestBaseClass(unittest.TestCase):
+    """
+    Contains multiple testing methods
+    """
+
+    def setUp(self):
+        """
+        To reset __nb_objects
+        """
+        Base._Base__nb_objects = 0
+
+    def test_setValidid(self):
+        """
+        Creating Base object with valid int id
+        """
+        object1 = Base(45)
+        self.assertEqual(object1.id, 45)
+
+    def test_setValidid2(self):
+        """
+        Creating Base object with a string id
+        """
+        object2 = Base("string")
+        self.assertEqual(object2.id, "string")
+
+    def test_setNoneid(self):
+        """
+        Creating Base object with None as id
+        """
+        object3 = Base(None)
+        self.assertEqual(object3.id, 1)
+
+    def test_setNoneid2(self):
+        """
+        Creating Base object with None as id mulitple times
+        """
+        object4 = Base(None)
+        self.assertEqual(object4.id, 1)
+        object4.id = "bleh"
+        self.assertEqual(object4.id, "bleh")
+        object4 = Base(None)
+        self.assertEqual(object4.id, 2)
+
+if __name__ == '__main__':
+    unittest.main()
