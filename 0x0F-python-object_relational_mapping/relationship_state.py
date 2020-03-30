4f5f04af47b7a86d218574b2054a1d671b824438
Michelle Giraldo <michellegsld@gmail.com>
2020-03-30 11:18:06 -0400

    [Initial] Add cities relationship to City() class

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Task 15:
+relationship_state.py
+Improve the model_state.py file
+
+Uses:
+100-relationship_states_cities.sql
+"""
+from sys import argv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    A new SQL table
+    """
+    __tablename__ = "states"
+    id = Column("id", Integer(), autoincrement=True,
+                nullable=False, primary_key=True)
+    name = Column("name", String(128), nullable=False)
+    cities = relationship('City', backref='State')
