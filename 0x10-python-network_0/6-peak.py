61f397c0fa8342d1bb483e1def931467ed8d2aa2
Michelle Giraldo <michellegsld@gmail.com>
2020-04-07 17:08:26 -0400

    [Initial] Finds the first peak in a list of unsorted integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+6-peak.py
+"""
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers
+    """
+    ints = list_of_integers
+    num = None
+    for i in range(len(ints)):
+        num = ints[i]
+        if i == 0:
+            if num >= ints[i + 1]:
+                break
+        if i < len(ints) - 1:
+            if num >= ints[i + 1] and num >= ints[i - 1]:
+                break
+        if i == len(ints) - 1:
+            if num >= ints[i - 1]:
+                break
+    return num
