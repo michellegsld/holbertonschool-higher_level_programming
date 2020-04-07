b2ff38225a0536b8740f0dd0a9264301a9fbefe3
Michelle Giraldo <michellegsld@gmail.com>
2020-04-07 19:49:05 -0400

    [Update] Try to get efficency check

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,16 +9,22 @@ def find_peak(list_of_integers):
     Finds a peak in a list of unsorted integers
     """
     ints = list_of_integers
+    length = len(list_of_integers)
+
+    if ints is None or length == 0:
+        return None
+
     num = None
-    for i in range(len(ints)):
+
+    if ints[0] >= ints[1]:
+        return ints[0]
+    if ints[length - 1] >= ints[length - 2]:
+        return ints[length - 1]
+
+    for i in range(1, length - 1):
         num = ints[i]
-        if i == 0:
-            if num >= ints[i + 1]:
-                break
         if i < len(ints) - 1:
             if num >= ints[i + 1] and num >= ints[i - 1]:
                 break
-        if i == len(ints) - 1:
-            if num >= ints[i - 1]:
-                break
+
     return num
