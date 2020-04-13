983ebc765572b574f4544b9271483b76f1bb95d6
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 17:59:17 -0400

    [Fix] Add if statement to check if X-Request-Id: is actually in it

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -11,6 +11,8 @@ from urllib import request
 if __name__ == "__main__":
     req = request.Request(argv[1])
     with request.urlopen(req) as response:
-        body = str(response.info()).split("X-Request-Id: ")[1]
-        answer = body.split('\n')[0]
-    print(answer)
+        body = str(response.info())
+        if "X-Request-Id: " in body:
+            body = body.split("X-Request-Id: ")[1]
+            answer = body.split('\n')[0]
+            print(answer)
