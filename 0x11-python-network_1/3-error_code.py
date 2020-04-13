796911b4f4ac69c5abd94a0a0b1bfd6af738c74c
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 19:08:11 -0400

    [Update] Decode the response read

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     try:
         req = request.Request(argv[1])
         with request.urlopen(req) as response:
-            print(response.read())
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as err:
         print("Error code: {}".format(err.code))
