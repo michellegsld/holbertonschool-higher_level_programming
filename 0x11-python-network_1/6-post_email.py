2fc731254f8a282d6a4d706f7febf33a2d654597
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 19:47:03 -0400

    [Update] Send a requests.post with email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,9 @@ To display the body of the response
 
 6-post_email.py
 """
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    
+    req = requests.post(argv[1], data = {'email': argv[2]})
+    print(req.text)
