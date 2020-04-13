00f47d7b95f2e919610f9f8fddbe54043ef4b379
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 19:47:50 -0400

    [Fix] Follow pep8 conventions

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,5 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    req = requests.post(argv[1], data = {'email': argv[2]})
+    req = requests.post(argv[1], data={'email': argv[2]})
     print(req.text)
