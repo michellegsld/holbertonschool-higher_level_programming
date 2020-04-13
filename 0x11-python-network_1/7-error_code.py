dee0ab5b14cb4cbc00eee88cad66e5bb5bba62f7
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 19:51:00 -0400

    [Update] Depending on status code, prints it or body of response

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,6 +5,12 @@ Sends a request and displays the body of the response
 
 7-error_code.py
 """
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    
+    req = requests.get(argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
