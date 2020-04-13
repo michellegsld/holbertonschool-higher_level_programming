3ee36b450d8f0b42d95c3a041db50a0276b8c187
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 19:37:00 -0400

    [Update] Import argv and requests to use .headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,9 @@ Sends a request and displays the value of X-Request-Id
 
 5-hbtn_header.py
 """
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    
+    req = requests.get(argv[1])
+    print(req.headers["X-Request-Id"])
