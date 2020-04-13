eada69a4e5cbf5616f2a1986557394d192dc9aa3
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 19:28:50 -0400

    [Update] Import requests and use requests.get

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,10 @@ Fetches https://intranet.hbtn.io/status
 
 4-hbtn_status.py
 """
+import requests
 
 if __name__ == "__main__":
-    
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
