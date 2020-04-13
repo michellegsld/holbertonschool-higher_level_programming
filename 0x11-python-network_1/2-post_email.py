75abc61d378ed74cdcb041282942ddb780d213ec
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 18:38:25 -0400

    [Update] Add values to send with URL and parse

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,6 +6,17 @@ To display body of the response (decoded in utf-8)
 
 2-post_email.py
 """
+from sys import argv
+from urllib import request, parse
 
 if __name__ == "__main__":
-    
+
+    value = {'email' : argv[2]}
+    data = parse.urlencode(value)
+    data = data.encode('utf-8')
+
+    url = request.Request(argv[1], data)
+
+    with request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print(body)
