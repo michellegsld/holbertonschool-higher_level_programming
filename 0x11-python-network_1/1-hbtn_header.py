e879237173bef430b8a803086585988fc3de7fa2
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 17:57:02 -0400

    [Update] Use .info instead of .read (from task 0) in order to find id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,12 @@ Displays value of the X-Request-Id variable
 
 1-hbtn_header.py
 """
+from sys import argv
+from urllib import request
 
 if __name__ == "__main__":
-    
+    req = request.Request(argv[1])
+    with request.urlopen(req) as response:
+        body = str(response.info()).split("X-Request-Id: ")[1]
+        answer = body.split('\n')[0]
+    print(answer)
