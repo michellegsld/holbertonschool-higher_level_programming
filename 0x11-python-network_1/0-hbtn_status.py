9aacab1f8871df4acad12aa99ef83482e89858d2
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 15:47:21 -0400

    [Update] Add code, try to use urlopen and read

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,8 @@ Fetch https://intranet.hbtn.io/status
 
 0-hbtn_status.py
 """
+from urllib import request
 
 if __name__ == "__main__":
-    
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        print(response.read())
