b8059729443af81d3649a527aacfc91044c87fb0
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 16:44:17 -0400

    [Update] Have to actually specify what to print, doesnt print output automatically

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,10 @@ Fetch https://intranet.hbtn.io/status
 from urllib import request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        print(response.read())
+    req = request.Request('https://intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
