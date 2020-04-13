22ad77c6b2287fe12edf6624ed98dc4fa546b87f
michelle <michellegsld@gmail.com>
2020-04-13 03:47:19 -0400

    [Initial] Empty file with basic outline in order to pull on other computer

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Task 2:
+Sends POST request with an email as a parameter
+To display body of the response (decoded in utf-8)
+
+2-post_email.py
+"""
+
+if __name__ == "__main__":
+    
