ce1b0ee780a568d7981b5856e8b4e45f9731c0ae
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 22:20:21 -0400

    [Update] Unite url and username then use a get request to find id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,16 @@ Use the Github API to display the id
 
 10-my_github.py
 """
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    
+    url = "https://api.github.com/users/" + argv[1]
+    req = requests.get(url)
+try:
+    if req.json():
+        print("{}".format(req.json()["id"]))
+    else:
+        print("None")
+except:
+    pass
