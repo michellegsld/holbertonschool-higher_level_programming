500bc04d8462d176d63a783e06dc91678cdf916b
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 22:21:34 -0400

    [Update] Fix indentation and print None if goes into except

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Task 10:
+Task 9:
 Use the Github API to display the id
 
 10-my_github.py
@@ -11,10 +11,10 @@ from sys import argv
 if __name__ == "__main__":
     url = "https://api.github.com/users/" + argv[1]
     req = requests.get(url)
-try:
-    if req.json():
-        print("{}".format(req.json()["id"]))
-    else:
+    try:
+        if req.json():
+            print("{}".format(req.json()["id"]))
+        else:
+            print("None")
+    except:
         print("None")
-except:
-    pass
