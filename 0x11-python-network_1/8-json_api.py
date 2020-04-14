504fa5ba5700105e04052ea74a0e5e319fbe84b1
Michelle Giraldo <michellegsld@gmail.com>
2020-04-13 20:15:44 -0400

    [Update] Use try/except to try and get JSON info

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,6 +5,18 @@ Sends POST request to http://0.0.0.0:5000/search_user
 
 8-json_api.py
 """
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    
+    q = ""
+    if len(argv) > 1:
+        q = argv[1]
+    req = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    try:
+        if req.json():
+            print("[{}] {}".format(req.json()["id"], req.json()["name"]))
+        elif len(req.json()) is 0:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
