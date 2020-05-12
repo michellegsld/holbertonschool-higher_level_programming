d198b1f1568466c0241954eb3c8b7efc96c3291c
Michelle Giraldo <michellegsld@gmail.com>
2020-05-11 23:24:38 -0400

    [Update] Inherit 5-square instead of 4-rectangle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
