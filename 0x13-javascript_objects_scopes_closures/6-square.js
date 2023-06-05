e355e099d855da465e1c8a0b2a669161c5b6227b
michellegsld <michellegsld@gmail.com>
2023-06-05 12:58:53 -0400

    [Update] Make Square inherit from 4-rectangle instead of 5-square, meaning the constructor needed to be added as well

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./5-square');
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  charPrint (c) {
+    constructor (size) {
+      super(size, size);
+    }
+
+    charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
