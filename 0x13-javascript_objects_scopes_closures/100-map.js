a6dea2a5f68bdb9cceea65309952dfff6168b857
michellegsld <michellegsld@gmail.com>
2023-06-05 13:46:31 -0400

    Separate into more variables for clarity

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-var oldList = Object.values(require('./100-data'))[0];
+const objList = require('./100-data');
 
+var oldList = Object.values(objList)[0];
 console.log(oldList);
 const newList = oldList.map((x, index) => (x * index));
 console.log(newList);
