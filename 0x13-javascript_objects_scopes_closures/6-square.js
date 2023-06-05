8d2eb1a06f7cb20811978cced17a634f9db3b28d
michellegsld <michellegsld@gmail.com>
2023-06-05 13:00:28 -0400

    [Fix] Run semistandard check and fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,18 +2,21 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-    constructor (size) {
-      super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint (c) {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
+
     let shape = '';
+
     for (let w = 0; w < this.width; w++) {
       shape = shape + c;
     }
+
     for (let h = 0; h < this.height; h++) {
       console.log(shape);
     }
