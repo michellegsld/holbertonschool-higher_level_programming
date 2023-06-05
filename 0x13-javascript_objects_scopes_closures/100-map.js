0df09479b74b23d9d9a28706e0c07dd1b8b9309a
michellegsld <michellegsld@gmail.com>
2023-06-05 15:10:17 -0400

    Try to find the requirement being failed... may be lack of import

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,8 @@
 
 const objList = require('./100-data');
 
-var oldList = Object.values(objList)[0];
+const oldList = Object.values(objList)[0];
 console.log(oldList);
+
 const newList = oldList.map((x, index) => (x * index));
 console.log(newList);
