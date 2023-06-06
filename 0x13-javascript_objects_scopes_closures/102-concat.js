b147adad062e23586958d9c671a70f3780a3e984
michellegsld <michellegsld@gmail.com>
2023-06-05 22:10:15 -0400

    Add basic if statement making sure needed values are not null

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, inputD) => {
-  if (err) throw err;
-  fs.readFile(process.argv[3], (err, inputD2) => {
+if (process.argv[2] && process.argv[3] && process.argv[4]) {
+  fs.readFile(process.argv[2], (err, inputD) => {
     if (err) throw err;
-    if (process.argv[4]) {
-      fs.writeFile(process.argv[4], inputD.toString() + '\n' + inputD2.toString() + '\n', (err, inputD3) => {
-        if (err) throw err;
-      });
-    }
+    fs.readFile(process.argv[3], (err, inputD2) => {
+      if (err) throw err;
+      if (process.argv[4]) {
+        fs.writeFile(process.argv[4], inputD.toString() + '\n' + inputD2.toString() + '\n', (err, inputD3) => {
+          if (err) throw err;
+        });
+      }
+    });
   });
-});
+}
