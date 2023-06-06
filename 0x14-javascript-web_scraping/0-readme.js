d07a13fc7dca49edf85c1b47dbe84a4f8bc2a749
michellegsld <michellegsld@gmail.com>
2023-06-05 22:22:09 -0400

    Get a different type of error message output but use readFile and process.stdout.write to display content

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (err, content) => {
+  if (err) throw err;
+  process.stdout.write(content);
+});
